c302fd94de54e39863d80ad5d29d81a023d1eafc
N. Dinio <nika1667@gmail.com>
2017-06-29 19:45:22 -0400

    Initial table creation and DB script added.

--- RealmData.sql
+++ RealmData.sql
@@ -1,2 +1,23 @@
-IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'RealmData')
-	CREATE DATABASE [RealmData];
+USE RealmData;
+
+
+CREATE TABLE ConnectedRealms(
+	ConnectID INT IDENTITY(100, 1),
+	RealmName VarChar(20) 
+	);
+	
+
+CREATE TABLE RealmList(
+	RealmID INT IDENTITY(200,1) PRIMARY KEY,
+	RealmType VARCHAR(20),
+	RealmPop VARCHAR(10),
+	RealmQueue BIT,
+	RealmStatus BIT,
+	RealmName VARCHAR(20),
+	RealmSlug VARCHAR(20),
+	Battlegroup VARCHAR(40),
+	locale VARCHAR(10),
+	timezone VARCHAR(40),
+	ConnectID INT
+);
+
