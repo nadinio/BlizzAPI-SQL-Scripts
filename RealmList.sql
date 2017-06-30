4cb3281e121db071d3a866ed83c9a961e4fba79d
N. Dinio <nika1667@gmail.com>
2017-06-30 00:43:03 -0400

    Remove deprecated file.

--- RealmList.sql
+++
@@ -1,8 +0,0 @@
-IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'RealmData')
-	CREATE DATABASE [RealmData];
-
-USE RealmData;
-
-CREATE TABLE RealmList(
-	RealmID INT IDENTITY(1,1) PRIMARY KEY
-);
